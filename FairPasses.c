781e3038f26b3c1a203f4e13be5eed1ec74bb039
Yashas Khot <109984055+yashaskhot@users.noreply.github.com>
2022-10-03 01:00:31 +0530

    CodeChef

---
+++ FairPasses.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+	int t,i,n,k;
+	
+	scanf("%d",&t);
+	
+	for(i = 0; i < t; i++){
+	    
+	    scanf("%d %d",&n,&k);
+	    
+	    if(k > n ){
+	        printf("\nYES");
+	    }
+	    
+	    else{
+	        
+	        printf("\nNO");
+	    }
+	    
+    
+	    
+	}
+	return 0;
+}
+
