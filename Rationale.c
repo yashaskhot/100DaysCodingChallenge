410891e315d2695e7c4c0d2626c7e83f6c641050
Yashas Khot <109984055+yashaskhot@users.noreply.github.com>
2022-10-08 19:47:05 +0530

    CodeChef

---
+++ Rationale.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int x,y;
+	
+	scanf("%d %d",&x,&y);
+	
+	if(x > y){
+	    
+	    printf("%d",x);
+	}
+	else{
+	    printf("%d",y);
+	    
+	}
+	
+	return 0;
+}
+
