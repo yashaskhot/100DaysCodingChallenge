f5cdc21496e2541329389561160d0c6abd363560
Yashas Khot <109984055+yashaskhot@users.noreply.github.com>
2022-10-09 11:23:25 +0530

    Codechef

---
+++ H or C.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+	
+	int t,i,c;
+	
+	scanf("%d",&t);{
+	    
+	    for(i = 0; i < t; i++){
+	        
+	        scanf("%d",&c);
+	        
+	        if(c > 20){
+	            
+	            printf("\nHOT");
+	        }
+	        else{
+	            printf("\nCOLD");
+	            
+	        }
+	    }
+	    
+	}
+	return 0;
+}
+
