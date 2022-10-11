58ec4f8dfc7fdf616fb9d3ddf2e1fcaa5b572782
Yashas Khot <109984055+yashaskhot@users.noreply.github.com>
2022-10-11 22:49:14 +0530

    CodeChef

---
+++ FillBucket.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t,i,k,x,d;
+	
+	scanf("%d",&t);
+	
+	for(i = 0;i < t;i++){
+	    
+	    scanf("%d %d",&k,&x);
+	    d = k-x;
+	    printf("\n%d",d);
+	    
+	    
+	}
+	return 0;
+}
+
